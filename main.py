6ead9c949b10f55bde7d13a5fefde124b3fbbd6b
zhengsheng <zhengsheng@xinyunlian.com>
2018-06-21 16:12:58 +0800

    wx test project

---
+++ main.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+
+from wordcloud import WordCloud
+import numpy
+from PIL import Image
+import matplotlib.pyplot as plt
+import itchat
+
+users = ''
+SEARCH = 'LAG'
+itchat.login()
+room = itchat.search_chatrooms(SEARCH)
+room_full = itchat.update_chatroom(room[0]['UserName'], detailedMember=True)
+for usr in room_full['MemberList']:
+    print usr['NickName']
+    users += ' ' + usr['NickName']
+print users
+
+sig = users
+msk = numpy.array(Image.open('c:/temp/logo.jpg'))
+wc = WordCloud(font_path='STHUPO.TTF',
+               background_color='white',
+               max_font_size=65,
+               min_font_size=15,
+#               mask=msk,
+               font_step=1,
+               mode='RGB',
+               random_state=45,
+               width=1250,
+               height=1000,
+               margin=15)
+wc.generate(sig)
+plt.imshow(wc,interpolation='bilinear')
+plt.axis('off')
+plt.savefig('c:/temp/wc.jpg', dpi=200)
+
+itchat.send_image('c:/temp/wc.jpg', 'filehelper')
---
+++ test_toll.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+# File :${name}.py
+from unittest import TestCase
+
+from main import Toll
+
+
+class TestToll(TestCase):
+    t1=Toll()
+    t1.go()
+
