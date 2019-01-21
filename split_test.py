73f4930cb9b95eadff4ea3896d8ff14f11745ae3
zhengsheng <zhengsheng@xinyunlian.com>
2019-01-21 15:20:59 +0800

    try to write class

--- split_test.py
+++ split_test.py
@@ -7,14 +7,14 @@ chun_100k = 100 * kilobytes
 megabytes = kilobytes * 1000
 
 
-def split(fromdir, todir, chunksize = chun_100k):
+def split(fromfile, todir, chunksize = chun_100k):
     if not os.path.exists(todir):
         os.mkdir(todir)
     else:
         for fname in os.listdir(todir):
             os.remove(os.path.join(todir, fname))
     partnum = 0
-    input = open(fromdir, 'rb')
+    input = open(fromfile, 'rb')
     while True:
         chunk = input.read(chunksize)
         if not chunk:
@@ -28,5 +28,23 @@ def split(fromdir, todir, chunksize = chun_100k):
     assert partnum <= 9999
     return partnum
 
+def join(fromdir, tofile):
+    output = open(tofile, 'wb')
+    parts = os.listdir(fromdir)
+    parts.sort()
+    for fname in parts:
+        filepath = os.path.join(fromdir, fname)
+        fileobj = open(filepath, 'rb')
+        while True:
+            filebytes = fileobj.read(chun_100k)
+            if not filebytes:
+                break
+            output.write(filebytes)
+        fileobj.close()
+    output.close()
+
+
+
 if __name__ == '__main__':
-   split('d:/zs/test1/1.jpg', 'd:/zs/test1/jpg_split')
+   #split('d:/zs/test1/1.jpg', 'd:/zs/test1/jpg_split')
+    join('d:/zs/test1/jpg_split', 'd:/zs/test1/2.jpg')
