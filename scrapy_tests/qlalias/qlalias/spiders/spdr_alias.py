af3351d53a0314d566370d21b624bcf5b65fa3f3
zhengsheng <zhengsheng@xinyunlian.com>
2019-04-08 15:50:01 +0800

    test node

--- scrapy_tests/qlalias/qlalias/spiders/spdr_alias.py
+++ scrapy_tests/qlalias/qlalias/spiders/spdr_alias.py
@@ -7,50 +7,44 @@ from datetime import datetime
 class SpdrAliasSpider(scrapy.Spider):
     name = 'spdr_alias'
     allowed_domains = ['qlstats.net']
-    start_urls = ['https://qlstats.net/players?page=6920']
-
-    base_url = "https://qlstats.net/players?page={}"
-    page_num = 6920
     GMT_FORMAT = '%a, %d %b %Y %H:%M:%S UTC'
+
+    num = 0
+    all_players = []
+    with open('d:/list.txt', encoding='utf-8') as f:
+        for line in f.readlines():
+            lst = line.split(',')
+            sid = lst[0]
+            all_players.append(sid)
+    start_urls = ['https://qlstats.net/aliases/{}'.format(all_players[0])]
+
     def parse(self, response):
-        items = []
-        sel = response.xpath("//div[@class='row']//tbody/tr")
-        for sub in sel:
-            item = QlaliasItem()
-            item['player_id'] = sub.xpath("./td/text()").extract_first()
-            item['nick'] = sub.xpath("./td//span[@class='ql7']/text()").extract_first()
-            if item['nick'] == 'Anonymous':
-                continue
-            else:
-                print(item['nick'], ' of ', self.page_num)
-            tmp = sub.xpath("./td[3]/span/@title").extract_first()
-            tmp = datetime.strptime(tmp, self.GMT_FORMAT).strftime("%Y-%m-%d %H:%M:%S")
-            item["joined"] = tmp
-            sid_url = "https://qlstats.net/player/" + item['player_id']
-            yield scrapy.Request(url=sid_url, meta={"item":item}, callback=self.sid_parse)
-
-        if self.page_num <= 10774:
-            self.page_num += 1
-            new_url = self.base_url.format(self.page_num)
-            time.sleep(1)
-            yield scrapy.Request(url=new_url, callback=self.parse)
-
-
-    def sid_parse(self, response):
-        item = response.meta["item"]
-        sid = response.xpath("//div[@class='row']/div[@class='col-xs-6 col-sm-4 col-md-3']//p//a/text()").extract_first()
-        item["steam_id"] = sid
-        alias_url = "https://qlstats.net/aliases/{}".format(sid)
-        yield scrapy.Request(url=alias_url, meta={"item":item}, callback=self.alias_parse)
-
-    def alias_parse(self, response):
-        item = response.meta["item"]
+        item = QlaliasItem()
+        item['steam_id'] = response.xpath("//div[@class='row']//div[@class='col-xs-12']/a/text()").extract_first()
         alst = []
         aliases = response.xpath("//div[@class='row']//tbody/tr")
         for a in aliases:
-            alias = a.xpath("./td/span/text()").extract_first()
-            if alias is not None:
-                alst.append(alias)
+            als = a.xpath("./td").extract()
+            for tmp in als:
+                if 'tdcenter' in tmp:
+                    continue
+                tmp = tmp.replace("<td>", "").replace("</td>", "").replace("</span>", "")
+                tmp = tmp.replace('<span class="ql1">', '')
+                tmp = tmp.replace('<span class="ql2">', '')
+                tmp = tmp.replace('<span class="ql3">', '')
+                tmp = tmp.replace('<span class="ql4">', '')
+                tmp = tmp.replace('<span class="ql5">', '')
+                tmp = tmp.replace('<span class="ql6">', '')
+                tmp = tmp.replace('<span class="ql7">', '')
+                tmp = tmp.replace('<span class="ql8">', '')
+                tmp = tmp.replace('<span class="ql9">', '')
+                if tmp is not None:
+                    alst.append(tmp)
         item["aliases"] = '，'.join(alst)
 
-        return item
+        yield item
+
+        if self.num < len(self.all_players):
+            self.num += 1
+            new_url = "https://qlstats.net/aliases/{}".format(self.all_players[self.num])
+            yield scrapy.Request(url=new_url, callback=self.parse)
