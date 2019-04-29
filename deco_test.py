3512625bd1f6c9be5b74e7f104cc8317f30c2867
zhengsheng <zhengsheng@xinyunlian.com>
2019-04-29 15:43:42 +0800

    test logger

--- deco_test.py
+++ deco_test.py
@@ -2,15 +2,30 @@
 __author__ = 'zs'
 from datetime import datetime
 
+#decorator with no parms
 def log(func):
     def wrapper(*args, **kwargs):
         print("calling {}".format(func.__name__))
         return func(*args, **kwargs)
     return wrapper
 
+#decorator with parms
+def log2(text):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            print("callling with {}{}".format(text, func.__name__))
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
 @log
 def now():
     print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
+@log2('(this is log2)')
+def now2():
+    print(datetime.now().strftime("%Y-%m-%d"))
+
 if __name__ == '__main__':
     now()
+    now2()
--- log_test.py
+++ log_test.py
@@ -1,15 +1,34 @@
 # coding=utf-8
 __author__ = 'zs'
 import logging
+import logging.handlers
+import time
 
-logging.basicConfig(level=logging.INFO,
-                    filename='wtf.log',
-                    filemode='a',
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger('testLogger')
-
-if __name__ == '__main__':
+def test1():
+    logging.basicConfig(level=logging.INFO,
+                        filename='wtf.log',
+                        filemode='a',
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger('testLogger')
+    con = logging.StreamHandler()#default to standard output
+    con.setLevel(logging.ERROR)
+    logger.addHandler(con)
     for a in range(10):
         logger.info("this is info - {}".format(a))
     for b in range(10):
         logger.error("this is error - {}".format(b))
+
+def test2():
+    logging.basicConfig(level=logging.DEBUG,
+                        filemode='a',
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+    fh = logging.handlers.RotatingFileHandler('wtf2.log', 'S', 2, 10)
+    fh.suffix = "%Y%m%d-%H%M%S.log"
+    logger.addHandler(fh)
+    i = 0
+    while i < 1000:
+        logger.info(time.strftime("%Y-%m-%d %H%M%S", time.localtime()))
+        time.sleep(1)
+if __name__ == '__main__':
+    test2()
